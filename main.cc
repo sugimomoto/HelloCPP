fb84f0f079a9aac2c2bcb15f408dc5a51f3b3c55
sugimomoto <sugimomoto@gmail.com>
2022-01-07 11:42:36 +0900

    first commit

---
+++ main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Hello. Please input your name" << endl;
+    cin >> name;
+
+    cout << "Hey " << name << "-san Good morning";
+
+    return 0;
+}
